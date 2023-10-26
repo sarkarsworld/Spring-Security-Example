3bcbbf5e6f057f1fe981a0c24d4623b3fd5da8ca
aloks <aloks@Dell>
2023-10-26 22:27:00 +0530

    added lombok.

--- Spring-Security-Example/src/main/java/com/simplesolutions/models/User.java
+++ Spring-Security-Example/src/main/java/com/simplesolutions/models/User.java
@@ -1,44 +1,18 @@
 package com.simplesolutions.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 	
 	private String username;
 	private String password;
 	private String email;
-	
-	public User() {
-		super();
-	}
-	
-	public User(String username, String password, String email) {
-		super();
-		this.username = username;
-		this.password = password;
-		this.email = email;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 	
 }
